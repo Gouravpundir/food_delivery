114ef280af411f69951973216b78adb51b4759f9
Gourav Pundir <107459689+Gouravpundir@users.noreply.github.com>
2023-03-13 22:49:00 +0530

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -13,7 +13,7 @@ export default function Cart() {
 
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
-    let response = await fetch("http://localhost:4000/orderData", {
+    let response = await fetch("https://vast-teal-clam-toga.cyclic.app/orderData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
